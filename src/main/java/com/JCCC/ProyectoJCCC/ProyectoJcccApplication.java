df3849e5f692db8d9f895241dc44ec0dfbccd99f
Santiago Garavito <jsgaravito90@mise4na.edu.co>
2021-07-09 17:45:08 -0500

    primer project

---
+++ src/main/java/com/JCCC/ProyectoJCCC/ProyectoJcccApplication.java
@@ -0,0 +1,13 @@
+package com.JCCC.ProyectoJCCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJcccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJcccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JCCC/ProyectoJCCC/ProyectoJcccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JCCC.ProyectoJCCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJcccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
